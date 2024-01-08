5e0c49418c9487cd3ccce6ebc7184ea8a18f9753
Joel Singh <joelsingh788@gmail.com>
2024-01-08 04:58:16 -0500

    Add AimWithLimelight command

---
+++ src/main/java/frc/robot/commands/AimWithLimelight.java
@@ -0,0 +1,37 @@
+package frc.robot.commands;
+
+import static java.lang.Math.abs;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.Limelight;
+
+public class AimWithLimelight extends Command {
+    final double TOLERANCE = 1;
+    final double SPEED = 0.2;
+    final private Drivetrain drivetrain;
+    final private Limelight limelight;
+
+    @Override
+    public void execute() {
+        double horizontalOffset = limelight.getHorizontalOffset();
+
+        if (horizontalOffset > TOLERANCE) {
+          drivetrain.arcadeDrive(0, -SPEED);
+        } else if (horizontalOffset < TOLERANCE) {
+          drivetrain.arcadeDrive(0, SPEED);
+        }
+    }
+
+    @Override
+    public boolean isFinished() {
+        return abs(limelight.getHorizontalOffset()) < TOLERANCE;
+    }
+
+    public AimWithLimelight(Drivetrain drivetrain, Limelight limelight) {
+        this.drivetrain = drivetrain;
+        this.limelight = limelight;
+        addRequirements(drivetrain);
+        withName("aim with limelight");
+    }
+}
