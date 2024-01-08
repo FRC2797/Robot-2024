400ce547f9b1e816a85d25b67dc571c233e1a209
Joel Singh <joelsingh788@gmail.com>
2024-01-08 05:02:46 -0500

    Add SwitchPipelineCommandThenAim

---
+++ src/main/java/frc/robot/commands/SwitchPipelineThenAim.java
@@ -0,0 +1,16 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.Limelight;
+import frc.robot.subsystems.Limelight.Pipeline;
+
+public class SwitchPipelineThenAim extends SequentialCommandGroup {
+    public SwitchPipelineThenAim(Limelight limelight, Drivetrain drivetrain, Pipeline pipeline) {
+        super(
+            limelight.switchPipelineCommand(pipeline),
+            new AimWithLimelight(drivetrain, limelight)
+        );
+    }
+}
