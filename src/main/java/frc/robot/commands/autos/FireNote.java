c1314778354f5586c7fb38834bcb599aa4d676bf
FRC 2797 <10475967+FRC-2797@users.noreply.github.com>
2024-01-24 17:14:28 -0500

    Add empty commands for unimplmented commands

---
+++ src/main/java/frc/robot/commands/autos/FireNote.java
@@ -0,0 +1,9 @@
+package frc.robot.commands.autos;
+
+import edu.wpi.first.wpilibj2.command.PrintCommand;
+
+public class FireNote extends PrintCommand {
+    public FireNote() {
+        super("Firing not implemented");
+    }
+}
---
+++ src/main/java/frc/robot/commands/autos/Intake.java
@@ -0,0 +1,9 @@
+package frc.robot.commands.autos;
+
+import edu.wpi.first.wpilibj2.command.PrintCommand;
+
+public class Intake extends PrintCommand {
+    public Intake() {
+        super("Intake not implemented");
+    }
+}
