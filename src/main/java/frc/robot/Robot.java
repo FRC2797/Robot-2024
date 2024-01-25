02271dfb0bef3b5f491c7e3f3f2b008624847c77
FRC 2797 <10475967+FRC-2797@users.noreply.github.com>
2024-01-25 16:23:56 -0500

    Add null guard to getAutonomousCommand

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,10 +5,8 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.commands.SwerveJoystick;
-import frc.robot.subsystems.SwerveDrivetrain;
 
 public class Robot extends TimedRobot {
   RobotContainer robotContainer = new RobotContainer();
@@ -23,7 +21,12 @@ public class Robot extends TimedRobot {
 
   @Override
   public void autonomousInit() {
-    robotContainer.getAutonomousCommand().schedule();
+    Command auto = robotContainer.getAutonomousCommand();
+    if (auto == null) {
+      System.out.println("No Auto command selected");
+    } else {
+        robotContainer.getAutonomousCommand().schedule();
+    }
   }
 
   @Override
