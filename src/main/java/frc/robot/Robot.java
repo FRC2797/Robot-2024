0b1e963306f7c3c0be2055aaa7fe635828c29b6d
FRC 2797 <team2797@gmail.com>
2024-02-01 12:22:39 -0500

    Change motors to have bevels facing left

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,33 +4,70 @@
 
 package frc.robot;
 
+import java.io.File;
+
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj.Preferences;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import swervelib.SwerveDrive;
+import swervelib.parser.SwerveParser;
+import swervelib.telemetry.SwerveDriveTelemetry;
+import swervelib.telemetry.SwerveDriveTelemetry.TelemetryVerbosity;
 
 public class Robot extends TimedRobot {
-  RobotContainer robotContainer = new RobotContainer();
+  SwerveDrive swerveDrive;
+
   @Override
   public void robotInit() {
-  }
+    Preferences.initDouble("vx", 0);
+    Preferences.initDouble("vy", 0);
+    Preferences.initDouble("omega", 0);
 
-  @Override
-  public void robotPeriodic() {
-    CommandScheduler.getInstance().run();
-  }
-
-  @Override
-  public void autonomousInit() {
-    Command auto = robotContainer.getAutonomousCommand();
-    if (auto == null) {
-      System.out.println("No Auto command selected");
-    } else {
-        robotContainer.getAutonomousCommand().schedule();
+    // Configure the Telemetry before creating the SwerveDrive to avoid unnecessary
+    // objects being created.
+    SwerveDriveTelemetry.verbosity = TelemetryVerbosity.HIGH;
+    try {
+      swerveDrive = new SwerveParser(new File(Filesystem.getDeployDirectory(), "swerve")).createSwerveDrive(5);
+      // Alternative method if you don't want to supply the conversion factor via JSON
+      // files.
+      // swerveDrive = new SwerveParser(directory).createSwerveDrive(maximumSpeed,
+      // angleConversionFactor, driveConversionFactor);
+    } catch (Exception e) {
+      throw new RuntimeException(e);
     }
+    swerveDrive.setHeadingCorrection(false);
   }
 
   @Override
-  public void autonomousExit() {
-    CommandScheduler.getInstance().cancelAll();
+  public void testPeriodic() {
+    swerveDrive.drive(new ChassisSpeeds(
+        Preferences.getDouble("vx", 0),
+        Preferences.getDouble("vy", 0),
+        Preferences.getDouble("omega", 0)));
   }
+  // RobotContainer robotContainer = new RobotContainer();
+  // @Override
+  // public void robotInit() {
+  // }
+
+  // @Override
+  // public void robotPeriodic() {
+  // CommandScheduler.getInstance().run();
+  // }
+
+  // @Override
+  // public void autonomousInit() {
+  // Command auto = robotContainer.getAutonomousCommand();
+  // if (auto == null) {
+  // System.out.println("No Auto command selected");
+  // } else {
+  // robotContainer.getAutonomousCommand().schedule();
+  // }
+  // }
+
+  // @Override
+  // public void autonomousExit() {
+  // CommandScheduler.getInstance().cancelAll();
+  // }
 }
