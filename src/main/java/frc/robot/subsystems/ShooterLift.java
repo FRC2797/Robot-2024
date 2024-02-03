0baafdc9bf32f4494e247b639c483c6ee85d8b9e
FRC 2797 <10475967+FRC-2797@users.noreply.github.com>
2024-02-03 09:56:49 -0500

    Create shooter locking method

--- src/main/java/frc/robot/subsystems/ShooterLift.java
+++ src/main/java/frc/robot/subsystems/ShooterLift.java
@@ -1,6 +1,9 @@
 package frc.robot.subsystems;
 
+import java.beans.Encoder;
+
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -9,8 +12,23 @@ public class ShooterLift extends SubsystemBase {
     CANSparkMax motor = new CANSparkMax(9, MotorType.kBrushless);
     CANSparkMax motor2 = new CANSparkMax(10, MotorType.kBrushless);
 
+
     public ShooterLift() {
         
+    }
+
+    public void enableMotors(boolean on){
+        if (on){
+            motor.setIdleMode(IdleMode.kBrake);
+            motor2.setIdleMode(IdleMode.kBrake);
+        }
+    }
+
+    public void disableMotors(boolean off){
+        if (off){
+            motor.setIdleMode(IdleMode.kCoast);
+            motor2.setIdleMode(IdleMode.kCoast);
+        }
     }
 
     public void shooterUp(double speed) {
