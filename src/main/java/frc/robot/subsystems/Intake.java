e3ad72f4c60d4b496334445bb7c282392e848b1f
FRC 2797 <team2797@gmail.com>
2024-02-06 16:33:06 -0500

    Add methods to get intake commands

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -5,7 +5,12 @@ import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.BangBangController;
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.StartEndCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
@@ -13,6 +18,7 @@ public class Intake extends SubsystemBase {
     CANSparkMax motor = new CANSparkMax(11, MotorType.kBrushless);
     RelativeEncoder encoder = motor.getEncoder();
     BangBangController bangBang = new BangBangController();
+    AnalogInput distanceSensor = new AnalogInput(0);
 
     double setpoint = 0;
 
@@ -28,8 +34,28 @@ public class Intake extends SubsystemBase {
         );
     }
 
-    public void setSetpoint(double setpoint) {
-        this.setpoint = setpoint;
+    public void setSetpoint(double rpm) {
+        this.setpoint = rpm;
+    }
+
+    public Command intake(double rpm) {
+        return new StartEndCommand(() -> this.setSetpoint(rpm), () -> this.setSetpoint(0), this);
+    }
+
+    public Command intake() {
+        return intake(Constants.intakeDefaultRPM);
+    }
+
+    public Command intakeUntilFullyIn() {
+        double distanceWhenFullyIn = 999;
+        double tolerance = 1;
+        return intake(30).until(
+            () -> MathUtil.isNear(distanceWhenFullyIn, getDistance(), tolerance)
+        );
+    }
+
+    public double getDistance() {
+        return distanceSensor.getValue();
     }
 
     public double getRotationsPerMinute() {
