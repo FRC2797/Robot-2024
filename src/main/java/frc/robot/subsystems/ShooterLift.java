f8626f4d34379214e024ca6e770c8a0b1ba841c2
FRC 2797 <team2797@gmail.com>
2024-02-06 15:42:51 -0500

    Change ShooterLift to use PIDSubsystem

--- src/main/java/frc/robot/subsystems/ShooterLift.java
+++ src/main/java/frc/robot/subsystems/ShooterLift.java
@@ -1,49 +1,44 @@
 package frc.robot.subsystems;
 
-import java.beans.Encoder;
-
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
 
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.PIDSubsystem;
 import frc.robot.Constants;
 
-public class ShooterLift extends SubsystemBase {
+public class ShooterLift extends PIDSubsystem {
     CANSparkMax motor = new CANSparkMax(9, MotorType.kBrushless);
     CANSparkMax motor2 = new CANSparkMax(10, MotorType.kBrushless);
 
     RelativeEncoder encoder = motor.getEncoder();
     RelativeEncoder encoder2 = motor2.getEncoder();
 
-    PIDController pidController = new PIDController(0.4, 0, 0);
-    double setpoint = 0;
-
-
     public ShooterLift() {
+        super(new PIDController(0.4, 0, 0));
         encoder.setPositionConversionFactor(Constants.kShooterLiftGearRatio);
         encoder2.setPositionConversionFactor(Constants.kShooterLiftGearRatio);
     }   
 
-    @Override
-    public void periodic() {
-        double output = pidController.calculate(getPosition(), setpoint);
-        motor.set(output);
-        motor2.set(output);
-    }
-
     // Setpoint is from 0 to 1
     public void setPosition(double setpoint) {
-        this.setpoint = MathUtil.clamp(setpoint, 0, 1);
+        this.setSetpoint(MathUtil.clamp(setpoint, 0, 1));
     }
 
     // From 0 to 1
-    public double getPosition() {
+    @Override
+    public double getMeasurement() {
         double encoderReadingsAveraged = (encoder.getPosition() + encoder2.getPosition()) / 2;
         return encoderReadingsAveraged / Constants.kShooterLiftMaxRotations;
+    }
+
+    @Override
+    protected void useOutput(double output, double setpoint) {
+        motor.set(output);
+        motor2.set(output);
     }
 
     public void enableMotors(boolean on){
