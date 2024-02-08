9b21303c05f00f645d8422a92fca61172865d5f3
FRC 2797 <team2797@gmail.com>
2024-02-08 15:24:06 -0500

    Working ShooterLift PIDSubsystem

--- src/main/java/frc/robot/subsystems/ShooterLift.java
+++ src/main/java/frc/robot/subsystems/ShooterLift.java
@@ -7,22 +7,61 @@ import com.revrobotics.RelativeEncoder;
 
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.PIDSubsystem;
+import edu.wpi.first.wpilibj2.command.StartEndCommand;
 import frc.robot.Constants;
 
 public class ShooterLift extends PIDSubsystem {
-    CANSparkMax motor = new CANSparkMax(9, MotorType.kBrushless);
-    CANSparkMax motor2 = new CANSparkMax(10, MotorType.kBrushless);
+    CANSparkMax left = new CANSparkMax(9, MotorType.kBrushless);
+    CANSparkMax right = new CANSparkMax(10, MotorType.kBrushless);
 
-    RelativeEncoder encoder = motor.getEncoder();
-    RelativeEncoder encoder2 = motor2.getEncoder();
+    RelativeEncoder leftEncoder = left.getEncoder();
+    RelativeEncoder rightEncoder = right.getEncoder();
+
+    PIDController pid = new PIDController(1.3, 0.1, 0);
+
+    ShuffleboardTab tab = Shuffleboard.getTab("ShooterLift");
 
     public ShooterLift() {
-        super(new PIDController(0.4, 0, 0));
-        encoder.setPositionConversionFactor(Constants.kShooterLiftGearRatio);
-        encoder2.setPositionConversionFactor(Constants.kShooterLiftGearRatio);
+        super(getPidController());
+
+        leftEncoder.setPositionConversionFactor(1 / Constants.kShooterLiftGearRatio);
+        rightEncoder.setPositionConversionFactor(1 / Constants.kShooterLiftGearRatio);
+
+
+        left.setInverted(true);
+        right.setInverted(false);
+
+        leftEncoder.setPosition(0);
+        leftEncoder.setPosition(0);
+
         brakeMotors();
-    }   
+        tab.addDouble("Current measurement", this::getMeasurement);
+        tab.addDouble("Current setpoint", this::getSetpoint);
+        tab.addDouble("Current left speed", left::get);
+        tab.addDouble("Current right speed", right::get);
+
+        tab.addDouble("left encoder value", leftEncoder::getPosition);
+        tab.addDouble("right encoder value", rightEncoder::getPosition);
+
+        tab.add(this.getGoToPositionCommand(0).withName("Go to 0%"));
+        tab.add(this.getGoToPositionCommand(0.1).withName("Go to 10%"));
+        tab.add(this.getGoToPositionCommand(0.2).withName("Go to 20%"));
+        tab.add(this.getGoToPositionCommand(0.3).withName("Go to 30%"));
+        tab.add(this.getGoToPositionCommand(0.4).withName("Go to 40%"));
+        tab.add(this.getGoToPositionCommand(0.5).withName("Go to 50%"));
+        tab.add(this.getGoToPositionCommand(0.6).withName("Go to 60%"));
+        tab.add(this.getGoToPositionCommand(0.7).withName("Go to 70%"));
+        tab.add(this.getGoToPositionCommand(0.8).withName("Go to 80%"));
+        tab.add(this.getGoToPositionCommand(0.9).withName("Go to 90%"));
+        tab.add(this.getGoToPositionCommand(1).withName("Go to 100%"));
+
+
+        this.disable();
+    }
 
     // Setpoint is from 0 to 1
     public void setPosition(double setpoint) {
@@ -32,18 +71,45 @@ public class ShooterLift extends PIDSubsystem {
     // From 0 to 1
     @Override
     public double getMeasurement() {
-        double encoderReadingsAveraged = (encoder.getPosition() + encoder2.getPosition()) / 2;
+        double encoderReadingsAveraged = (leftEncoder.getPosition() + rightEncoder.getPosition()) / 2;
         return encoderReadingsAveraged / Constants.kShooterLiftMaxRotations;
     }
 
     @Override
     protected void useOutput(double output, double setpoint) {
-        motor.set(output);
-        motor2.set(output);
+        left.set(output);
+        right.set(output);
     }
 
     public void brakeMotors(){
-        motor.setIdleMode(IdleMode.kBrake);
-        motor2.setIdleMode(IdleMode.kBrake);
+        left.setIdleMode(IdleMode.kBrake);
+        right.setIdleMode(IdleMode.kBrake);
+    }
+
+    public Command getGoToPositionCommand(double setpoint) {
+        Command goToPosition = new StartEndCommand(
+            () -> {
+                this.setPosition(setpoint);
+                this.enable();
+            },
+            () -> {
+                this.setPosition(0);
+                this.disable();
+            },
+            this
+        );
+
+        return goToPosition;
+    }
+
+    public void unbrakeMotors(){
+        left.setIdleMode(IdleMode.kCoast);
+        right.setIdleMode(IdleMode.kCoast);
+    }
+
+    private static PIDController getPidController() {
+        PIDController pid = new PIDController(0.7, 0.2, 0);
+        pid.setTolerance(0.05);
+        return pid;
     }
 }
