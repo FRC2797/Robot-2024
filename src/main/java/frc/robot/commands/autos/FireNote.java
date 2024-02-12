7c9b6547ebdc5203237e9347dd8b62e2a5a81cb2
FRC 2797 <team2797@gmail.com>
2024-02-12 17:04:29 -0500

    Change Firenote to be a parallelRaceGroup

--- src/main/java/frc/robot/commands/autos/FireNote.java
+++ src/main/java/frc/robot/commands/autos/FireNote.java
@@ -7,17 +7,17 @@ import frc.robot.subsystems.ShooterLift;
 
 import static edu.wpi.first.wpilibj2.command.Commands.*;
 
-public class FireNote extends SequentialCommandGroup {
+import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
+
+public class FireNote extends ParallelRaceGroup {
     public FireNote(double height, double shooterRPM, Intake intake, Shooter shooter, ShooterLift shooterLift) {
         super(
-            race(
-                shooterLift.getGoToPositionCommand(height),
-                shooter.getGoToRPMCommand(shooterRPM),
-                sequence(
-                    waitUntil(() -> shooterLift.atSetpoint()),
-                    waitUntil(() -> shooter.atSetpoint()),
-                    intake.intakeIntoShooter()
-                )
+            shooterLift.getGoToPositionCommand(height),
+            shooter.getGoToRPMCommand(shooterRPM),
+            sequence(
+                waitUntil(() -> shooterLift.atSetpoint()),
+                waitUntil(() -> shooter.atSetpoint()),
+                intake.intakeIntoShooter()
             )
         );
     }
