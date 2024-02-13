ddc8b31022f3798edacc87a20adcdd99c7a27554
FRC 2797 <team2797@gmail.com>
2024-02-13 17:24:13 -0500

    Format

--- src/main/java/frc/robot/commands/autos/FireNote.java
+++ src/main/java/frc/robot/commands/autos/FireNote.java
@@ -1,13 +1,14 @@
 package frc.robot.commands.autos;
 
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import static edu.wpi.first.wpilibj2.command.Commands.none;
+import static edu.wpi.first.wpilibj2.command.Commands.sequence;
+import static edu.wpi.first.wpilibj2.command.Commands.waitUntil;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Shooter;
 import frc.robot.subsystems.ShooterLift;
-
-import static edu.wpi.first.wpilibj2.command.Commands.*;
-
-import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 
 public class FireNote extends ParallelRaceGroup {
     public FireNote(double height, double shooterRPM, Intake intake, Shooter shooter, ShooterLift shooterLift) {
