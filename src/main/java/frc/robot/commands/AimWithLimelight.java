7a8be8d4e0e1ad289f0d76a926bdd82c5e8fa54e
FRC 2797 <team2797@gmail.com>
2024-02-14 19:09:49 -0500

    Change aimWithLimelight to use driveToRotation

--- src/main/java/frc/robot/commands/AimWithLimelight.java
+++ src/main/java/frc/robot/commands/AimWithLimelight.java
@@ -1,10 +1,9 @@
 package frc.robot.commands;
 
+import static edu.wpi.first.math.util.Units.degreesToRadians;
+
 import java.util.Set;
 
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.DeferredCommand;
 import frc.robot.subsystems.Limelight;
 import frc.robot.subsystems.SwerveDrivetrain;
@@ -13,7 +12,7 @@ public class AimWithLimelight extends DeferredCommand {
     public AimWithLimelight(SwerveDrivetrain drivetrain, Limelight limelight) {
         super(() -> {
             double rotationToBeAimed = drivetrain.getPose().getRotation().getDegrees() + limelight.getHorizontalOffset();
-            return drivetrain.driveToPoseRelativeToCurrent(new Pose2d(new Translation2d(), Rotation2d.fromDegrees(rotationToBeAimed)), true);
+            return drivetrain.driveToRotation(degreesToRadians(rotationToBeAimed));
         }, Set.of(drivetrain));
     }
 }
