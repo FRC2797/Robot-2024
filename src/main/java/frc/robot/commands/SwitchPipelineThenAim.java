8c73a0d930d2f1fa13d7e766633eb8426dac9b33
FRC 2797 <team2797@gmail.com>
2024-02-14 19:04:47 -0500

    Remove unnecessary files

--- src/main/java/frc/robot/commands/SwitchPipelineThenAim.java
+++ src/main/java/frc/robot/commands/SwitchPipelineThenAim.java
@@ -2,7 +2,6 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.Limelight;
 import frc.robot.subsystems.SwerveDrivetrain;
 import frc.robot.subsystems.Limelight.Pipeline;
