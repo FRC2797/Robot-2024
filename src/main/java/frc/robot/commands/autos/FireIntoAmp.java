69d774f82e3456fa89b7cb17a0f151b3b01355bd
FRC 2797 <team2797@gmail.com>
2024-02-14 19:59:22 -0500

    Have fireIntoAmp aim before starting

--- src/main/java/frc/robot/commands/autos/FireIntoAmp.java
+++ src/main/java/frc/robot/commands/autos/FireIntoAmp.java
@@ -1,14 +1,17 @@
 package frc.robot.commands.autos;
 
+import frc.robot.commands.AimWithLimelight;
 import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Limelight;
 import frc.robot.subsystems.Shooter;
 import frc.robot.subsystems.ShooterLift;
+import frc.robot.subsystems.SwerveDrivetrain;
 
 public class FireIntoAmp extends FireNote {
     final private static double kHeightToFireAt = 0.95;
     final private static double kShooterRPMToFireAt = 500;
 
-    public FireIntoAmp(Intake intake, Shooter shooter, ShooterLift shooterLift) {
+    public FireIntoAmp(Intake intake, Shooter shooter, ShooterLift shooterLift, SwerveDrivetrain drivetrain, Limelight limelight) {
         super(
             kHeightToFireAt,
             kShooterRPMToFireAt,
@@ -16,5 +19,6 @@ public class FireIntoAmp extends FireNote {
             shooter,
             shooterLift
         );
+        beforeStarting(new AimWithLimelight(drivetrain, limelight));
     }
 }
