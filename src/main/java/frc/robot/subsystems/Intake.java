7e7a7ef1158a589cd187a6f572313749dfb90200
FRC 2797 <team2797@gmail.com>
2024-02-21 20:33:11 -0500

    Update motor ids

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -15,7 +15,7 @@ import edu.wpi.first.wpilibj2.command.StartEndCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Intake extends SubsystemBase {
-    protected static CANSparkMaxSimWrapper motor = new CANSparkMaxSimWrapper(11, MotorType.kBrushless);
+    protected static CANSparkMaxSimWrapper motor = new CANSparkMaxSimWrapper(13, MotorType.kBrushless);
     RelativeEncoder encoder = motor.getEncoder();
     BangBangController bangBang = new BangBangController();
     ShuffleboardTab tab = Shuffleboard.getTab("Intake");
