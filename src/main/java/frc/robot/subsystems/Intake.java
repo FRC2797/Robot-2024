bb712c7182886298b6907dc133237e92b75981e2
FRC 2797 <team2797@gmail.com>
2024-02-24 13:17:40 -0500

    Add test commands to intake

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -2,7 +2,6 @@ package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel.MotorType;
-import com.revrobotics.CANSparkMax;
 import com.revrobotics.ColorSensorV3;
 import com.revrobotics.RelativeEncoder;
 
@@ -30,6 +29,19 @@ public class Intake extends SubsystemBase {
         tab.addBoolean("Is the ring in?", this::noteIsIn);
         tab.addDouble("Current proximity", this::getProximity);
         tab.addDouble("Current encoder position", encoder::getPosition);
+
+        tab.add(this.getGoToPowerCommand(0).withName("Go to 0%"));
+        tab.add(this.getGoToPowerCommand(0.1).withName("Go to 10%"));
+        tab.add(this.getGoToPowerCommand(0.2).withName("Go to 20%"));
+        tab.add(this.getGoToPowerCommand(0.3).withName("Go to 30%"));
+        tab.add(this.getGoToPowerCommand(0.4).withName("Go to 40%"));
+        tab.add(this.getGoToPowerCommand(0.5).withName("Go to 50%"));
+        tab.add(this.getGoToPowerCommand(0.6).withName("Go to 60%"));
+        tab.add(this.getGoToPowerCommand(0.7).withName("Go to 70%"));
+        tab.add(this.getGoToPowerCommand(0.8).withName("Go to 80%"));
+        tab.add(this.getGoToPowerCommand(0.9).withName("Go to 90%"));
+        tab.add(this.getGoToPowerCommand(1).withName("Go to 100%"));
+
     }
 
     public Command intake(double speed) {
