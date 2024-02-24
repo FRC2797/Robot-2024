381c08db85bdb593296013b896d2e44cd08d93f3
Joel Singh <joelsingh788@gmail.com>
2024-02-24 00:11:02 -0500

    Remove unnecessary lines in LimelightSim

--- src/main/java/frc/robot/subsystems/simulated/LimelightSim.java
+++ src/main/java/frc/robot/subsystems/simulated/LimelightSim.java
@@ -15,16 +15,6 @@ public class LimelightSim extends Limelight {
   private final int ENTRY_NOT_FOUND = -9999;
 
   ShuffleboardTab tab = Shuffleboard.getTab("Limelight");
-  public enum Pipeline {
-    placeholder(0),
-    anotherPlaceholder(1);
-
-    public final int value;
-
-    Pipeline(int value) {
-      this.value = value;
-    }
-  }
 
   public double horizontalOffset = 0;
   public double getHorizontalOffset() {
@@ -45,56 +35,6 @@ public class LimelightSim extends Limelight {
   @Override
   protected void switchPipeline(Pipeline pipeline) {
     currentPipeline = pipeline;
-  }
-
-  public Command switchPipelineCommand(Pipeline pipeline) {
-    return runOnce(() -> switchPipeline(pipeline)).andThen(waitSeconds(0.1));
-  }
-
-  public String getSeenAprilTagAsString() {
-    switch (getSeenAprilTag()) {
-      case ENTRY_NOT_FOUND:
-        return "No Apriltag";
-
-      case 1:
-        return "Blue's right source";
-
-      case 2:
-        return "Blue's left source";
-
-      case 3:
-        return "Red's right subwoofer";
-
-      case 4:
-        return "Red's middle subwoofer";
-
-      case 5:
-        return "Red's Amp";
-
-      case 6:
-        return "Blue's Amp";
-
-      case 7:
-        return "Blue's middle subwoofer";
-
-      case 8:
-        return "Blue's left subwoofer";
-
-      case 9:
-        return "Red's right source";
-
-      case 10:
-        return "Red's left source";
-
-      case 11, 12, 13:
-        return "Red stage";
-
-      case 14, 15, 16:
-        return "Blue stage";
-    
-      default:
-        return "April tag pipeline broken";
-    }
   }
 
   public int seenAprilTag = 1;
