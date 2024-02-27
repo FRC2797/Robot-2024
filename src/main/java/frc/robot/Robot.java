b0517cb841f0d0d45a417a2738097a442447a886
FRC 2797 <team2797@gmail.com>
2024-02-27 11:25:23 -0500

    Stop data logger

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -19,6 +19,8 @@ public class Robot extends TimedRobot {
       if (dataLoggingEnabled) {
         DataLogManager.start();
         URCL.start();
+      } else {
+        DataLogManager.stop();
       }
   }
 
